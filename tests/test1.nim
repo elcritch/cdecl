61e1befe9c21d20511b545bfb7c947f527f3d3d3
Jaremy Creechley <creechley@gmail.com>
2022-06-05 02:35:49 -0700

    initial setup

--- tests/test1.nim
+++ tests/test1.nim
@@ -6,7 +6,25 @@
 # To run these tests, simply execute `nimble test`.
 
 import unittest
+import strutils, strformat
+import cdecl
 
+type
+  c_var = object
+  c_var_other_t = object
+
+template CDefineVar*(name: untyped, size: static[int]) =
+  var name* {.inject, importc, nodecl.}: ptr c_var_other_t
+  {.emit: "/*TYPESECTION*/ C_DEFINE_VAR($1, $2);" % [ symbolName(name), $size, ] .}
+
+const cVarSz = 1024
+CDefineVar(myVar, cVarSz)
+var blink {.exportc.}: c_var
+ 
 import cdecl
 test "can add":
   check add(5, 5) == 10
+
+  ##   KDefineStack(blinkStack, blinkStackSz.int)
+  ##   var blink {.exportc.}: k_thread
+
